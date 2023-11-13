5b98a334fcaa8473fcf1c490d789c81b9c58d39c
strongdever <kstronger77@gmail.com>
2023-11-13 09:58:37 +0900

    202311130958

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,61 +1,82 @@
-!(function ($) {
-    "use strict";    
-    var curr_Index = 0;
-    $(".picker-before").click(function() {
-        curr_Index--;
-        if( curr_Index < 0 ) curr_Index += 3;
-        $(".pickup img").prop('src', $(".picker img:nth-child(" + (curr_Index + 1) + ")").prop('src')).prop('srcset', $(".picker img:nth-child(" + (curr_Index + 1) + ")").prop('srcset'));
-    })
-    
-    $(".picker-next").click(function() {
-        curr_Index++;
-        curr_Index = curr_Index % 3;
-        $(".pickup img").prop('src', $(".picker img:nth-child(" + (curr_Index + 1) + ")").prop('src')).prop('srcset', $(".picker img:nth-child(" + (curr_Index + 1) + ")").prop('srcset'));
-    })
-
-    // picker
-    function pickupImage() {
-        const $this = $(this);
-        const currentIndex = $this.data('index');
-        curr_Index = currentIndex;
-        const $imagePicker = $this.parent();
-        const $images = $imagePicker.find('img');
-        const maxIndex = $imagePicker.data('max-index');
-        const $target = $('.' + $imagePicker.data('target'));
-        var newIndex = currentIndex;
-
-        if ($target.length > 0) {
-            $target.prop('src', $this.prop('src')).prop('srcset', $this.prop('srcset'));
-        }
+function searchFunction() {
+    var input, filter, ul, li, s, p, i, txtValue;
+    // input = document.getElementById('myInput');
+    // filter = input.value.toUpperCase();
+    ul = document.getElementById("faq-container");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      p = li[i].getElementsByTagName("p")[0];
+      s = li[i].getElementsByTagName("h4")[0];
+      txtValue = (s.textContent || s.innerText) + " " + (p.textContent || p.innerText) ;
+    //   if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    //     li[i].style.display = "";
+    //   } else {
+    //     li[i].style.display = "none";
+    //   }
     }
-    $('.picker').each(function (index, element) {
-        const $imagePicker = $(element);
-        var lastIndex = 0;
-
-        $imagePicker.find('img').each(function (index, img) {
-            $(img).data('index', index);
-            lastIndex = index;
-        });
-        $imagePicker.data('maxIndex', lastIndex);
+  }
+  
+  filterSelection("サービス概要について")
+  function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("q-a-item");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+      faqRemoveClass(x[i], "show");
+      if (x[i].className.indexOf(c) > -1) faqAddClass(x[i], "show");
+    }
+  }
+  
+  // Show filtered elements
+  function faqAddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) == -1) {
+        element.className += " " + arr2[i];
+      }
+    }
+  }
+  
+  // Hide elements that are not selected
+  function faqRemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1); 
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+  
+  // Add active class to the current control button (highlight it)
+  var btnContainer = document.getElementById("faqBtnContainer");
+  var btns = btnContainer.getElementsByClassName("btn-faq");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
     });
-    $('.picker img').on('click', pickupImage);
-
-    // lightbox
-    const $lightbox = $('#lightbox');
-    const $lightboxImage = $('#lightbox img');
-    const $lightboxSource = $('.photos .pickup img, img.lightbox');
-
-    $lightboxSource.on('click', function () {
-        const $this = $(this);
-
-        $('body').css({ 'overflow-y': 'hidden' });
-        $lightboxImage.css({ opacity: 0.0 }).prop('src', $this.prop('src')).prop('srcset', $this.prop('srcset'));
-        $lightbox.css({ display: 'flex' });
-        $lightboxImage.css({ opacity: 1.0 });
-    });
-    $lightbox.on('click', function () {
-        $('body').css({ 'overflow-y': 'scroll' });
-        $lightbox.css({ display: 'none' });
-        $lightboxImage.css({ opacity: 0.0 });
-    });
-})(jQuery);
+  }
+  
+  
+  // Accordion Function
+  $(function() {
+      // (Optional) Active an item if it has the class "is-active"	
+      $("#faq-container > .q-a-item.is-active").children(".answer").slideDown();
+      
+      $("#faq-container > .q-a-item > h4").click(function() {
+      //event.preventDefault();
+          // Cancel the siblings
+          $(this).closest('li').siblings(".q-a-item").removeClass("is-active").children(".answer").slideUp();
+          // Toggle the item
+          $(this).closest('li').toggleClass("is-active").children(".answer").slideToggle("ease-out");
+      });
+  });
